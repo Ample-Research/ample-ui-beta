72515edb5f4782a9a248a1179ee5238a52f2cc7b
kellydinneen <kelly.dinneen@slalom.com>
2023-09-01 16:01:48 -0500

    Remove Deprecated import

--- src/components/AuthProvider.jsx
+++ src/components/AuthProvider.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import firebaseApp from '../utils/firebaseconfig';
 import AuthContext from './AuthContext';
 
